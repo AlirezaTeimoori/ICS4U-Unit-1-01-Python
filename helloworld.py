aad4cfe8bef7256aa51e0700ccaca72fd9ba7304
alireza <alirezateimoori80@gmail.com>
2019-02-08 01:09:59 -0500

    create helloworld.py file and write the code

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#---------------------------------------#
+#-- Created by:     Alireza Teimoori  --#
+#-- Created on:     Feb 8 2019        --#
+#-- Created for:    Unit 1-01         --#
+#-- Course Code:    ICS4U             --#
+#-- Teacher Name:   Chris Atkinson    --#
+#---------------------------------------#
+
+print("Hello, World!")
